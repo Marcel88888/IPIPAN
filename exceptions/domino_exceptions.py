23f24404dd4be4cb3cb6607f6427ebcf4f8f3452
Marcel88888 <marcel.kawski1@gmail.com>
2021-03-19 16:57:42 +0100

    move_domino() function implemented. InvalidInputDataTypesError, EmptyInputStringError and NotAllowableCharError added.

---
+++ exceptions/domino_exceptions.py
@@ -0,0 +1,17 @@
+class InvalidInputDataTypesError(Exception):
+    def __init__(self):
+        self.message = 'Error: Invalid input data types. Must be: (str, int:positive or 0).'
+        super().__init__(self.message)
+
+
+class EmptyInputStringError(Exception):
+    def __init__(self):
+        self.message = 'Error: Input string cannot be empty.'
+        super().__init__(self.message)
+
+
+class NotAllowableCharError(Exception):
+    def __init__(self, given_string):
+        self.__given_string = given_string
+        self.__message = f'Error: Given string ("{self.__given_string}") contains illicit characters.'
+        super().__init__(self.__message)
---
+++ exercises/domino.py
@@ -0,0 +1,35 @@
+from exceptions import domino_exceptions
+
+
+def move_domino(arrangement, iterations):
+    allowable_chars = ['|', '/', '\\']
+    if isinstance(arrangement, str) and isinstance(iterations, int) and iterations >= 0:
+        if arrangement != '':
+            if all([char in allowable_chars for char in arrangement]):  # checking if 'arrangement' contains
+                # only allowable characters.
+                arrangement_list = list(arrangement)
+                arr_list_bef_iter = arrangement_list.copy()
+                for iteration in range(iterations):
+                    for position, domino in enumerate(arrangement_list):
+                        if domino == '|':
+                            if 0 < position < len(arrangement_list) - 1:
+                                if arr_list_bef_iter[position-1] == '/' and arr_list_bef_iter[position+1] != '\\':
+                                    arrangement_list[position] = '/'
+                                elif arr_list_bef_iter[position+1] == '\\' and arr_list_bef_iter[position-1] != '/':
+                                    arrangement_list[position] = '\\'
+                            elif position == 0:
+                                if arr_list_bef_iter[position+1] == '\\':
+                                    arrangement_list[position] = '\\'
+                            elif position == len(arrangement_list) - 1:
+                                if arr_list_bef_iter[position-1] == '/':
+                                    arrangement_list[position] = '/'
+                    arr_list_bef_iter = arrangement_list.copy()
+                return "".join(arrangement_list)
+            raise domino_exceptions.NotAllowableCharError(arrangement)
+        raise domino_exceptions.EmptyInputStringError()
+    raise domino_exceptions.InvalidInputDataTypesError()
+
+
+print(move_domino('||//||\\||/\\|', 1))
+
+
